6e0582b882bd12e80d59c3eb1347ecd4b47ee40d
voruti <45130096+voruti@users.noreply.github.com>
2020-05-29 17:36:22 +0200

    Add 3 methods

--- src/main/java/voru/snake/IGui.java
+++ src/main/java/voru/snake/IGui.java
@@ -1,9 +1,35 @@
 package voru.snake;
 
 /**
- * @author Till
+ * @author voruti
  *
  */
 public interface IGui {
 
+	/**
+	 * Legt die Größe des Spielfeldes fest.
+	 * 
+	 * @param hoehe  die gewünschte Höhe
+	 * @param breite die gewünschte Breite
+	 */
+	void setzeGroesse(int hoehe, int breite);
+
+	/**
+	 * Füllt das entsprechende Spielfeld mit dem Text {@code str}.
+	 * 
+	 * @param x
+	 * @param y
+	 * @param str
+	 * @return den vorherigen Wert dieses Spielfeldes
+	 */
+	String setzeFeldAuf(int x, int y, String str);
+
+	/**
+	 * Fragt das entsprechende Spielfeld ab und gibt es zurück.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	String abfrageFeld(int x, int y);
 }
